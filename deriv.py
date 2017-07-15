46adfb2dee2a5738d22f56692bf430e3c93c18d1
Alex Vaschillo <alex.vaschillo@gmail.com>
2017-07-15 12:52:04 -0700

    added polynomial fit to deriv

--- deriv.py
+++ deriv.py
@@ -6,12 +6,21 @@ import numpy as np
 fin = "total_energy.dat"
 fout = "press.dat"
 column = 1
+deg = 40 #degree of polynomial fit
 
 data = np.loadtxt(fin,skiprows=1)
-output = np.zeros((data.shape[0]-1,2))
+output = np.zeros((data.shape[0]-1,3))
+myfit=np.polyfit(data[:,0],data[:,1],deg)	
+print(myfit)
+
 for i in xrange(data.shape[0]-1):
+	#compute derivative manually
 	output[i,1] = (data[i+1,column]-data[i,column])/(data[i+1,0]-data[i,0])
 	output[i,0] = 0.5*(data[i+1,0]+data[i,0])
+	
+	#compute derivative from polynomial fit
+	for j in xrange(deg+1):
+		output[i,2] = output[i,2] + (deg-j)*myfit[j]*(output[i,0]**(deg-1-j))
 
 np.savetxt(fout,output)
 
